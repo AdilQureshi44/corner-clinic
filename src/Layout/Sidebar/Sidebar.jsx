f1a2f86ddec6fcd1d2aec4fe8731412ec95f9216
Adil Qureshi <adil.aq48@gmail.com>
2025-04-12 09:44:44 +0530

    fix the header and sidebar collapsed issued

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -1,31 +1,48 @@
-import "./Sidebar.css"
-import {useState} from "react";
+import "./Sidebar.css";
+import { useState } from "react";
 import {
-    IconDashboard, IconFile, IconChartBar,
+    IconDashboard,
+    IconFile,
+    IconChartBar,
+    IconBrandAppstore,
 } from "@tabler/icons-react";
 
-const Sidebar = ({isSidebarCollapsed}) => {
+const Sidebar = ({ isSidebarCollapsed }) => {
     const listItem = [
-        {name: "Dashboard", icon: <IconDashboard/>},
-        {name: "Pages", icon: <IconFile/>}, {
-        name: "Charts", icon: <IconChartBar/>},
+        { name: "Dashboard", icon: <IconDashboard /> },
+        { name: "Pages", icon: <IconFile /> },
+        { name: "Charts", icon: <IconChartBar /> },
     ];
+
     const [activeIndex, setActiveIndex] = useState(0);
-    return <>
-        <div className={`${isSidebarCollapsed ? "collapsed" : "sidebar"}`}>
+
+    return (
+        <div className={`sidebar ${isSidebarCollapsed ? "collapsed" : ""}`}>
             <div className="logo">
-                Modernize
+               <div  className="logo-icon"> <IconBrandAppstore size={32}/></div>
+                <div className="logo-text">Brand AppStore</div>
             </div>
-            <div className={`${isSidebarCollapsed ? "sidebar-item-two" : "sidebar-item"}`}>
-                {listItem.map((item, index) => (<div
-                    key={index}
-                    className={`item-list ${activeIndex === index ? "active" : ""}`}
-                    onClick={() => setActiveIndex(index)}>
-                    <div className="sidebar-icon">{item.icon}</div>
-                   <div className={`${isSidebarCollapsed ? "sidebar-icon-name-two" : "sidebar-icon-name"}`}>{item.name}</div>
-                </div>))}
+
+            <div className={`sidebar-item ${isSidebarCollapsed ? "collapsed" : ""}`}>
+                {listItem.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`item-list ${activeIndex === index ? "active" : ""}`}
+                        onClick={() => setActiveIndex(index)}
+                    >
+                        <div className="sidebar-icon">{item.icon}</div>
+                        <div
+                            className={`sidebar-icon-name ${
+                                isSidebarCollapsed ? "collapsed" : ""
+                            }`}
+                        >
+                            {item.name}
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    </>
-}
+    );
+};
+
 export default Sidebar;
